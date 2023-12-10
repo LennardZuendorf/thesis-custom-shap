416d2e448915e199aaec9037ea5622fef4cc9ccb
LennardZuendorf <l.p.zuendorf@gmail.com>
2023-12-10 12:15:36 +0100

    feat: updated logging

--- shap/utils/transformers.py
+++ shap/utils/transformers.py
@@ -90,7 +90,7 @@ def is_transformers_lm(model):
     transformers_pretrained = safe_isinstance(model, "transformers.PreTrainedModel") or safe_isinstance(model, "transformers.TFPreTrainedModel")
     transformers_lm = safe_isinstance(model, MODELS_FOR_SEQ_TO_SEQ_CAUSAL_LM + MODELS_FOR_CAUSAL_LM)
 
-    logger.debug(f"transformers_pretrained: {transformers_pretrained}, transformers_lm: {transformers_lm}")
+    logger.debug(f"model: {model}, transformers_pretrained: {transformers_pretrained}, transformers_lm: {transformers_lm}")
 
     return transformers_pretrained and transformers_lm
 
